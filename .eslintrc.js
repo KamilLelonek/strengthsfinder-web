87dfd010f9205f7f12b6d11b819f497341ae4d3d
Kamil Lelonek <squixy.sln@gmail.com>
2018-11-23 21:51:46 +0100

    Add Vue plugin for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true
   },
   extends: ['plugin:vue/essential', '@vue/prettier'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'vue'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
