03c14e9b570eda18046d3dd096fff0ad64ef84cb
Kamil Lelonek <squixy.sln@gmail.com>
2018-11-27 22:10:44 +0100

    Improve store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,64 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import client from './api'
+
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+const state = {
+  name: null,
+  people: [],
+  isLoading: false,
+  error: null
+}
+
+function setState(name, people, isLoading, error) {
+  state.name = name
+  state.people = people
+  state.isLoading = isLoading
+  state.error = error
+}
+
+const mutations = {
+  startLoading(state, name) {
+    setState(name, [], true, null)
+  },
+  finishLoading(state, people) {
+    setState(null, people, false, null)
+  },
+  interruptLoading(state, error) {
+    setState(null, [], false, error)
+  }
+}
+
+const actions = {
+  load({commit}, name) {
+    commit('startLoading', name)
+
+    return client
+      .get(`/${name}`)
+      .then(({data}) => {
+        commit('finishLoading', data)
+        return true
+      })
+      .catch(function(error) {
+        if (error.response && error.response.status === 400) {
+          commit('interruptLoading', error.response.data)
+        } else {
+          commit('interruptLoading', error.message)
+        }
+        return false
+      })
+  }
+}
+
+const getters = {}
+
+const store = new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters
 })
+
+export default store
